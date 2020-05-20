c650a0f766e6af7d43e74462ce662109bbe912a4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-20 06:14:22 +0000

    Deploy cofacts/community-builder to github.com/cofacts/community-builder.git:gh-pages

---
+++ precache-manifest.bb6ff64f39f9bc903e361b76ffda4883.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821137df3262f5e3108634007a95eac5",
+    "url": "/community-builder/index.html"
+  },
+  {
+    "revision": "fcc6cca11953010d539a",
+    "url": "/community-builder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "057b61a7fcf995e4aaab",
+    "url": "/community-builder/static/js/2.f7533990.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/community-builder/static/js/2.f7533990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc6cca11953010d539a",
+    "url": "/community-builder/static/js/main.69b01a4e.chunk.js"
+  },
+  {
+    "revision": "20ce04a3e6dcf4d0d1fc",
+    "url": "/community-builder/static/js/runtime-main.707e18fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/community-builder/static/media/logo.5d5d9eef.svg"
+  }
+]);
