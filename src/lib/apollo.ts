8a22d1e6f20904ec3c422ce90eb39bdaaf11fe07
MrOrz <johnsonliang7@gmail.com>
2020-05-24 01:37:14 +0800

    Enable Apollo batch http

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,15 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { BatchHttpLink } from 'apollo-link-batch-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const client = new ApolloClient({
-  uri: `${process.env.REACT_APP_API_URL}/graphql`,
+  link: ApolloLink.from([
+    new BatchHttpLink({
+      uri: `${process.env.REACT_APP_API_URL}/graphql`,
+    }),
+  ]),
+  cache: new InMemoryCache(),
 });
 
 export default client;
