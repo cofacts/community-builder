f8d48bd44b0c845403db895e32d7516320191ba1
MrOrz <johnsonliang7@gmail.com>
2020-11-08 02:26:41 +0800

    BigNum page apollo 3 fix

--- src/pages/BigNum.tsx
+++ src/pages/BigNum.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
+import { useQuery, gql } from '@apollo/client';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { isSomeEnum } from '../lib/util';
