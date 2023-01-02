018b71dc67cf0c2979e0c739e46b8a8640922a12
MrOrz <johnsonliang7@gmail.com>
2023-01-02 16:45:52 +0800

    feat(APIStats): add filter editor drawer scaffold

--- src/pages/APIStats/APIStats.tsx
+++ src/pages/APIStats/APIStats.tsx
@@ -3,11 +3,14 @@ import { useApolloClient } from '@apollo/client';
 
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
 import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 
 import {
   ListArticleFilter,
@@ -77,8 +80,36 @@ function FilterText({ filter }: { filter: ListArticleFilter }) {
   return <>{names.join(', ') || 'All'}</>;
 }
 
+const FilterEditor = ({
+  filter,
+  onSubmit,
+}: {
+  filter: ListArticleFilter;
+  onSubmit: (filter: ListArticleFilter) => void;
+}) => {
+  const [currentFilter, setCurrentFilter] = useState(filter);
+
+  return (
+    <form
+      style={{ display: 'flex', flexFlow: 'column', height: '100%' }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit(currentFilter);
+      }}
+    >
+      <div style={{ flex: 1, overflowY: 'auto', padding: '8px' }}>
+        Form content here
+      </div>
+      <Button variant="contained" type="submit" sx={{ m: 1 }}>
+        Submit
+      </Button>
+    </form>
+  );
+};
+
 const APIStats = () => {
   useListAllCategoriesQuery();
+  const [editingFilterIdx, setEditingFilterIdx] = useState<number>();
 
   const [filters, setFilters] = useState<ListArticleFilter[]>(() => [
     {},
@@ -93,64 +124,88 @@ const APIStats = () => {
   ]);
 
   return (
-    <Stack spacing={4}>
-      {filters.map((filter, idx) => (
-        <div key={idx}>
-          <Stack component="header" spacing={1} direction="row">
-            <Typography variant="h5" sx={{ mb: 1, flex: 1 }}>
-              <FilterText filter={filter} />
-            </Typography>
-            {idx !== 0 && (
+    <>
+      <Stack spacing={4}>
+        {filters.map((filter, idx) => (
+          <div key={idx}>
+            <Stack component="header" spacing={1} direction="row">
+              <Typography variant="h5" sx={{ mb: 1, flex: 1 }}>
+                <FilterText filter={filter} />
+              </Typography>
+              <IconButton onClick={() => setEditingFilterIdx(idx)}>
+                <EditOutlinedIcon />
+              </IconButton>
+              {idx !== 0 && (
+                <IconButton
+                  onClick={() => {
+                    setFilters((fs) => [
+                      ...fs.slice(0, idx - 1),
+                      filter,
+                      fs[idx - 1],
+                      ...fs.slice(idx + 1),
+                    ]);
+                  }}
+                >
+                  <KeyboardArrowUpOutlinedIcon />
+                </IconButton>
+              )}
+              {idx < filters.length - 1 && (
+                <IconButton
+                  onClick={() => {
+                    setFilters((fs) => [
+                      ...fs.slice(0, idx),
+                      fs[idx + 1],
+                      filter,
+                      ...fs.slice(idx + 2),
+                    ]);
+                  }}
+                >
+                  <KeyboardArrowDownOutlinedIcon />
+                </IconButton>
+              )}
               <IconButton
                 onClick={() => {
                   setFilters((fs) => [
-                    ...fs.slice(0, idx - 1),
+                    ...fs.slice(0, idx + 1),
                     filter,
-                    fs[idx - 1],
                     ...fs.slice(idx + 1),
                   ]);
                 }}
               >
-                <KeyboardArrowUpOutlinedIcon />
+                <ContentCopyOutlinedIcon />
               </IconButton>
-            )}
-            {idx < filters.length - 1 && (
               <IconButton
                 onClick={() => {
-                  setFilters((fs) => [
-                    ...fs.slice(0, idx),
-                    fs[idx + 1],
-                    filter,
-                    ...fs.slice(idx + 2),
-                  ]);
+                  setFilters((fs) => fs.filter((_, i) => i !== idx));
                 }}
               >
-                <KeyboardArrowDownOutlinedIcon />
+                <DeleteOutlineOutlinedIcon />
               </IconButton>
-            )}
-            <IconButton
-              onClick={() => {
-                setFilters((fs) => [
-                  ...fs.slice(0, idx + 1),
-                  filter,
-                  ...fs.slice(idx + 1),
-                ]);
-              }}
-            >
-              <ContentCopyOutlinedIcon />
-            </IconButton>
-            <IconButton
-              onClick={() => {
-                setFilters((fs) => fs.filter((_, i) => i !== idx));
-              }}
-            >
-              <DeleteOutlineOutlinedIcon />
-            </IconButton>
-          </Stack>
-          <APIStatsOfFilter filter={filter} />
-        </div>
-      ))}
-    </Stack>
+            </Stack>
+            <APIStatsOfFilter filter={filter} />
+          </div>
+        ))}
+      </Stack>
+      <Drawer
+        anchor="right"
+        open={editingFilterIdx !== undefined}
+        onClose={() => setEditingFilterIdx(undefined)}
+      >
+        {editingFilterIdx !== undefined && (
+          <FilterEditor
+            filter={filters[editingFilterIdx] ?? {}}
+            onSubmit={(newFilter) => {
+              setFilters((fs) => {
+                const filters = [...fs];
+                filters[editingFilterIdx] = newFilter;
+                return filters;
+              });
+              setEditingFilterIdx(undefined);
+            }}
+          />
+        )}
+      </Drawer>
+    </>
   );
 };
 
