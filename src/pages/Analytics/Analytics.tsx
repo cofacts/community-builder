ffdaf6a66d9f2792d79b6382a5dfff2f7d5f92b4
MrOrz <johnsonliang7@gmail.com>
2023-02-21 14:49:44 +0800

    feat(Analytics): implemt ArticlesTable

--- src/pages/Analytics/Analytics.tsx
+++ src/pages/Analytics/Analytics.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
+import ArticlesTable from './ArticlesTable';
 import { useLoadAnalyticsQuery } from '../../types';
 
 function Analytics() {
   const { data, error } = useLoadAnalyticsQuery({
     variables: {
-      filter: { ids: ['38av08c46kqmx'] },
-      dateRange: { GTE: 'noew-2M' },
+      filter: { ids: ['1ijttxdp31nsd', '250oafg9kzopp', '2xpltyroe0qbi'] },
+      dateRange: { GTE: 'now-2M' },
     },
   });
 
-  return <>Data: {JSON.stringify(error || data)}</>;
+  return (
+    <>
+      <p>Data: {JSON.stringify(error || data)}</p>
+      <div style={{ height: 500 }}>
+        <ArticlesTable articleEdges={data?.ListArticles?.edges ?? []} />
+      </div>
+    </>
+  );
 }
 
 export default Analytics;
